a8eb316ca10545f5de4f5e4973bef76bf60df8c4
Akash Purohit <akashpurohit1235@gmail.com>
2025-05-28 20:33:18 +0530

    the initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-
 import './App.css';
-import { Button } from './components/ui/Button';
 
 function App() {
 
   return (
     <>
-      <Button size="lg" variant="primary" text="Share"/>
-      <Button size="lg" variant="secondary" text="Add Content"/>
+      
     </>
   )
 }
